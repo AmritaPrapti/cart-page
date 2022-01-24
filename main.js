1c9ab684597e8b84a77c2483ba335f038b59aafa
Amrita <amrita@gmail.com>
2022-01-25 00:09:09 +0600

    cart item remove

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log(1)
+let removeCartItem = document.getElementsByClassName('btn-danger');
+
+// console.log(removeCartItem);
+
+for(var i=0; i<removeCartItem.length; i++){
+    let button = removeCartItem[i];
+    
+    button.addEventListener('click', function(){
+        let buttons = button.parentElement.parentElement;
+        buttons.remove();
+        
+    })
+}
+
